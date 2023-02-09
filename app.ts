2a77780854b385333bb350f13025ecd58eba19d2
Adedipupo <ajokanola@terragonltd.com>
2023-02-09 02:10:46 +0100

    routes-setup

--- app.ts
+++ app.ts
@@ -2,16 +2,13 @@ import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
 import cookieParser from'cookie-parser';
-var logger = require('morgan');
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index';
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
