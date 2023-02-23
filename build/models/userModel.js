b1e936d44fbee79c6919705250d00aeedecd97b0
Adedipupo <ajokanola@terragonltd.com>
2023-02-23 01:00:56 +0100

    otp-worked

--- build/models/userModel.js
+++ build/models/userModel.js
@@ -6,19 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserModel = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const userSchema = new mongoose_1.default.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+    // name: {
+    //     type: String,
+    //     required: true,
+    //   },
+    //   email: {
+    //     type: String,
+    //     required: true,
+    //     unique: true,
+    //   },
+    //   password: {
+    //     type: String,
+    //     required: true,
+    //   },
     phoneNumber: {
         type: String,
         required: true,
